9e00c32c7d25337f5ed87fc5c9113a3a394e3092
Selvarajan Thangavel <selvarajan2006@gmail.com>
2024-12-24 09:12:44 +0530

    Initial Commit

---
+++ SwiftUI Animations Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Animations Demo
+//
+//  Created by Selvarajan on 24/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI Animations Demo/SwiftUI_Animations_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Animations_DemoApp.swift
+//  SwiftUI Animations Demo
+//
+//  Created by Selvarajan on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Animations_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
